cfed949cd8ed0a5d29aaa311fd500ff57e903582
laurpe <laura.perttila@edu.bc.fi>
2022-04-05 16:16:47 +0300

    first commit cleanup and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Plant } from "./types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const getData = async <T,>(url: string): Promise<T> => {
+    const response = await fetch(url);
+    return response.json() as Promise<T>;
+};
+
+const App = () => {
+    const [plants, setPlants] = useState<Plant[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getData<Plant[]>("plantData.json");
+            setPlants(result);
+        };
+        fetchData().catch((error) => console.error(error));
+    }, []);
+
+    return <div>sdfsdf</div>;
+};
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Plant {
+    name: string;
+    watered: string[];
+    wateringSchedule: string;
+    soil: string;
+    potSize: string;
+    repotted: string[];
+}
