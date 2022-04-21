4d5b483dd1c138baac76da51dafda7960885c817
laurpe <laura.perttila@edu.bc.fi>
2022-04-21 20:08:18 +0300

    calculate next watering day

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,13 +1,20 @@
 import { Plant } from "../types";
+import moment from "moment";
 
 const PlantCard = ({ plant }: { plant: Plant }) => {
+    const calculateNextWatering = (plant: Plant): string => {
+        const start = moment();
+        const end = moment(plant.lastWatered).add(plant.wateringCycle, "days");
+        return end.from(start);
+    };
+
     return (
         <>
             <div>
                 <h2>{plant.name}</h2>
                 <button type="button">water</button>
             </div>
-            <div>water next tuesday</div>
+            <div>water {calculateNextWatering(plant)}</div>
         </>
     );
 };
