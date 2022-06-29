ce5ce9746d13936fbe288ab9203d2c242e2dad0b
laurpe <laura.perttila@edu.bc.fi>
2022-06-29 16:47:51 +0300

    fix typescript error

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import "./index.css";
 import App from "./App";
 
 const container = document.getElementById("app");
-const root = createRoot(container!);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+//@ts-ignore: container is never null
+const root = createRoot(container);
 root.render(<App />);
