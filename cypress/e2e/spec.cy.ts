a4e3152abec82c0b1be428c9d95f716f867ce474
laurpe <laura.perttila@edu.bc.fi>
2022-07-05 10:45:59 +0300

    tsconfig with cypress ok

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
-describe("empty spec", () => {
-    it("passes", () => {
-        cy.visit("https://example.cypress.io");
+describe("Plant tracker", () => {
+    it("renders front page", () => {
+        cy.visit("http://localhost:3000");
+        cy.contains("Plant tracker");
     });
 });
