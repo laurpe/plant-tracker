16fe2a92f16aeeebd02f4c39ba5f66bb2ab583fe
laurpe <laura.perttila@edu.bc.fi>
2022-07-07 16:26:26 +0300

    cypress env variables working

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-    env: {
-        apiKey: process.env.REACT_APP_MONGO_API_KEY,
-        apiUrl: process.env.REACT_APP_MONGO_API_URL,
-        baseUrl: process.env.REACT_APP_BASE_URL,
-    },
-
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
