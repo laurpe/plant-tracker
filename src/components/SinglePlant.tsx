dc12d95f1b9cb2e450a2d7217dc63aebd5d614c6
laurpe <laura.perttila@edu.bc.fi>
2022-07-11 18:54:43 +0300

    edit and delete icons

--- src/components/SinglePlant.tsx
+++ src/components/SinglePlant.tsx
@@ -8,7 +8,11 @@ import styled from "styled-components";
 
 import Card from "./style/Generics/Card";
 import Column from "./style/Generics/Column";
+import Row from "./style/Generics/Row";
 import Subtitle from "./style/Generics/Subtitle";
+
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL as string;
 
@@ -19,6 +23,8 @@ const getData = async <T,>(url: string): Promise<T> => {
 
 const StyledCard = styled(Card)`
     height: 100%;
+    padding: 16px;
+    font-size: 1rem;
 `;
 
 const StyledColumn = styled(Column)`
@@ -29,7 +35,11 @@ const StyledColumn = styled(Column)`
 const SinglePlant = () => {
     const [plant, setPlant] = useState<Plant>({
         name: "",
-        growingMedium: { name: "", composition: [], id: "" },
+        growingMedium: {
+            name: "",
+            composition: [{ component: "", percentage: 100 }],
+            id: "",
+        },
         lastWatered: "",
         wateringCycle: 0,
         imageName: "",
@@ -52,9 +62,16 @@ const SinglePlant = () => {
         <Popup>
             <StyledCard>
                 <StyledColumn>
-                    <Subtitle>Plant info</Subtitle>
+                    <Row justifyContent="space-between">
+                        <Subtitle>Plant info</Subtitle>
+                        <Row>
+                            <EditIcon sx={{ fontSize: 30 }} />
+                            <DeleteIcon sx={{ fontSize: 30 }} />
+                        </Row>
+                    </Row>
+
                     <p>{plant.name}</p>
-                    <p>{plant.growingMedium?.name}</p>
+                    <p>{plant.growingMedium.name}</p>
                     <p>{plant.lastWatered}</p>
                     <p>{plant.wateringCycle}</p>
                 </StyledColumn>
