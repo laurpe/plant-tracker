ab645d17662a6923b61e4e4c9b96cc6403a47112
laurpe <laura.perttila@edu.bc.fi>
2022-07-11 18:04:55 +0300

    fetch single plant data

--- src/components/SinglePlant.tsx
+++ src/components/SinglePlant.tsx
@@ -1,7 +1,44 @@
 import Popup from "./style/Generics/Popup";
 
+import { Plant } from "../types";
+
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL as string;
+
+const getData = async <T,>(url: string): Promise<T> => {
+    const response = await fetch(`${baseUrl}/${url}`);
+    return response.json() as Promise<T>;
+};
+
 const SinglePlant = () => {
-    return <Popup></Popup>;
+    const [plant, setPlant] = useState<Plant>({
+        name: "",
+        growingMedium: "",
+        lastWatered: "",
+        wateringCycle: 0,
+        imageName: "",
+        id: "",
+    });
+    const id = useParams().id as string;
+
+    useEffect(() => {
+        const fetchPlant = async () => {
+            const result = await getData<Plant>(`plants/${id}`);
+            setPlant(result);
+        };
+
+        fetchPlant().catch(() => {
+            throw new Error("Fetch plant unsuccessful");
+        });
+    }, [id]);
+
+    return (
+        <Popup>
+            <p>{plant.name}</p>
+        </Popup>
+    );
 };
 
 export default SinglePlant;
