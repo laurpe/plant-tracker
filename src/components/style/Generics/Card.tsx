831e1856debb30d8a64c923a6ace2bd45a78f541
laurpe <laura.perttila@edu.bc.fi>
2022-07-11 23:56:26 +0300

    card border shadow

--- src/components/style/Generics/Card.tsx
+++ src/components/style/Generics/Card.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-    box-shadow: rgba(0, 0, 0, 0.2) 1px 1px 2px;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 3px 0.5px;
     background-color: white;
 `;
 
