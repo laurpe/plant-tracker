a9a5a28b9dc922052c0324259e547537ee2ba142
laurpe <laura.perttila@edu.bc.fi>
2022-07-15 15:07:17 +0300

    test for adding growing medium with name that already exists

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,7 +7,7 @@ describe("Plant tracker", () => {
 
 describe("When a new plant is added", () => {
     beforeEach(() => {
-        cy.clearDatabase();
+        cy.deletePlants();
     });
     it("the plant is rendered on the front page", () => {
         cy.visit("http://localhost:3000");
@@ -26,7 +26,7 @@ describe("When a new plant is added", () => {
 
 describe("When plant info is modified", () => {
     beforeEach(() => {
-        cy.clearDatabase();
+        cy.deletePlants();
         cy.visit("http://localhost:3000");
         cy.addPlant();
     });
@@ -57,7 +57,7 @@ describe("When plant info is modified", () => {
 
 describe("Plant actions", () => {
     beforeEach(() => {
-        cy.clearDatabase();
+        cy.deletePlants();
     });
     it("when plant is watered, new time to water shows", () => {
         cy.visit("http://localhost:3000");
@@ -71,9 +71,10 @@ describe("Plant actions", () => {
     });
 });
 
-describe("Growing mediums", () => {
+describe.only("Growing mediums", () => {
     beforeEach(() => {
-        cy.clearDatabase();
+        cy.deletePlants();
+        cy.deleteTestGrowingMedium();
     });
 
     it("When new growing medium is added, it shows in add plant form's list of growing mediums", () => {
@@ -104,30 +105,51 @@ describe("Growing mediums", () => {
         cy.get("#plant-growingMedium-select").select("customMix");
     });
 
-    // it("Growing medium components' percentage can't add up to more than 100", () => {
-    //     cy.visit("http://localhost:3000");
+    it("Growing medium components' percentage can't add up to more than 100", () => {
+        cy.visit("http://localhost:3000");
 
-    //     cy.get("#add-plant-form-btn").click();
-    //     cy.get("#add-growing-medium-btn").click();
+        cy.get("#add-plant-form-btn").click();
+        cy.get("#add-growing-medium-btn").click();
 
-    //     cy.contains("Add growing medium");
+        cy.contains("Add growing medium");
 
-    //     cy.get("#growing-medium-name-input").type("customMix");
-    //     cy.get("#growing-medium-component-1-select").select(1);
-    //     cy.get("#growing-medium-percentage-1-input").clear().type("30");
+        cy.get("#growing-medium-name-input").type("customMix");
+        cy.get("#growing-medium-component-1-select").select(1);
+        cy.get("#growing-medium-percentage-1-input").clear().type("30");
 
-    //     cy.get("#growing-medium-add-more-components-btn").click();
+        cy.get("#growing-medium-add-more-components-btn").click();
 
-    //     cy.get("#growing-medium-component-2-select").select(2);
-    //     cy.get("#growing-medium-percentage-2-input").clear().type("20");
+        cy.get("#growing-medium-component-2-select").select(2);
+        cy.get("#growing-medium-percentage-2-input").clear().type("20");
 
-    //     cy.get("#growing-medium-add-more-components-btn").click();
+        cy.get("#growing-medium-add-more-components-btn").click();
 
-    //     cy.get("#growing-medium-component-3-select").select(3);
-    //     cy.get("#growing-medium-percentage-3-input").clear().type("90");
+        cy.get("#growing-medium-component-3-select").select(3);
+        cy.get("#growing-medium-percentage-3-input").clear().type("90");
 
-    //     cy.get("#growing-medium-submit-btn").click();
+        cy.get("#growing-medium-submit-btn").click();
 
-    //     cy.contains("Components can't add up to more than 100%");
-    // });
+        cy.contains("Components can't add up to more than 100%");
+    });
+
+    it("Can't add growing medium with a name that already exists", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("#add-plant-form-btn").click();
+        cy.get("#add-growing-medium-btn").click();
+
+        cy.contains("Add growing medium");
+
+        cy.get("#growing-medium-name-input").type("customMix");
+        cy.get("#growing-medium-component-1-select").select(1);
+        cy.get("#growing-medium-submit-btn").click();
+
+        cy.get("#add-growing-medium-btn").click();
+
+        cy.get("#growing-medium-name-input").type("customMix");
+        cy.get("#growing-medium-component-1-select").select(3);
+        cy.get("#growing-medium-submit-btn").click();
+
+        cy.contains("Name already exists");
+    });
 });
