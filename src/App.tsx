ccd021cc345ba0f86ec1a1a86ae98e7864475745
laurpe <laura.perttila@edu.bc.fi>
2022-07-19 10:17:51 +0300

    hook to store data in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 
-import { Plant, GrowingMedium } from "./types";
+import { Plant, GrowingMedium, State } from "./types";
+
+import { useStorage } from "./hooks/useStorage";
 
 import Main from "./components/Main";
 import SinglePlant from "./components/SinglePlant";
@@ -19,6 +21,37 @@ const getData = async <T,>(url: string): Promise<T> => {
 const App = () => {
     const [plants, setPlants] = useState<Plant[]>([]);
     const [growingMediums, setGrowingMediums] = useState<GrowingMedium[]>([]);
+
+    const [storage, setStorageItems] = useStorage();
+
+    console.log(storage);
+
+    setStorageItems({
+        plants: [
+            {
+                name: "growing medium should be perlite",
+                growingMedium: "62c81da8eaf9abe6b26ee543",
+                lastWatered: "2022-04-04T18:19:51.584Z",
+                wateringCycle: 5,
+                id: "62d55369d9425e57513eb7a4",
+                imageName: "",
+            },
+        ],
+        growingMediums: [
+            {
+                name: "leca",
+                composition: [
+                    {
+                        component: "leca",
+                        percentage: 100,
+                    },
+                ],
+                id: "62c81d79eaf9abe6b26ee53b",
+            },
+        ],
+    });
+
+    console.log(storage);
 
     useEffect(() => {
         const fetchPlants = async () => {
---
+++ src/hooks/useStorage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { State } from "../types";
+
+export const useStorage = () => {
+    const [storage, setStorage] = useState<State>({
+        plants: [],
+        growingMediums: [],
+    });
+
+    useEffect(() => {
+        const state = localStorage.getItem("state");
+
+        if (state) {
+            const stateObject = JSON.parse(state) as State;
+
+            setStorage(stateObject);
+        } else {
+            localStorage.setItem(
+                "state",
+                JSON.stringify({ plants: [], growingMediums: [] })
+            );
+        }
+    }, []);
+
+    const setStorageItems = (items: State) => {
+        localStorage.setItem("state", JSON.stringify(items));
+    };
+
+    return [storage, setStorageItems] as const;
+};
