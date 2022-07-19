901a4ed7eccfd6c7b635ddcafa3344b3868a53c6
laurpe <laura.perttila@edu.bc.fi>
2022-07-19 11:02:53 +0300

    hook to add and remove growing mediums

---
+++ src/hooks/useGrowingMediums.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useStorage } from "./useStorage";
+
+import axios from "axios";
+
+import { GrowingMedium } from "../types";
+
+export const useGrowingMediums = () => {
+    const [growingMediums, setGrowingMediums] = useState<GrowingMedium[]>([]);
+    const [storage, setStorageItems] = useStorage();
+
+    useEffect(() => {
+        const baseUrl = process.env.REACT_APP_API_BASE_URL as string;
+
+        const fetchData = async () => {
+            const response = await axios.get<GrowingMedium[]>(
+                `${baseUrl}/growing-mediums`
+            );
+            setGrowingMediums(response.data);
+        };
+
+        try {
+            void fetchData();
+        } catch (error) {
+            throw new Error("Could not fetch growing mediums");
+        }
+    }, []);
+
+    const addGrowingMedium = (growingMedium: GrowingMedium) => {
+        setStorageItems({
+            ...storage,
+            growingMediums: [...growingMediums, growingMedium],
+        });
+    };
+
+    const removeGrowingMedium = (growingMedium: GrowingMedium) => {
+        const itemsToKeep = growingMediums.filter(
+            (item) => item.id !== growingMedium.id
+        );
+
+        setStorageItems({ ...storage, growingMediums: itemsToKeep });
+    };
+
+    return { addGrowingMedium, removeGrowingMedium };
+};
