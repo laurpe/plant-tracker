1d9f17b5fb262e769a8ddfa82fdd0883f352d37f
laurpe <laura.perttila@edu.bc.fi>
2022-07-20 10:13:20 +0300

    plants hook

---
+++ src/hooks/usePlants.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import { Plant } from "../types";
+import { useAPIData } from "./useAPIData";
+
+export const usePlants = () => {
+    const [plants, setPlants] = useState<Plant[]>([]);
+    const data = useAPIData<Plant[]>("plants");
+
+    useEffect(() => {
+        if (data) {
+            setPlants(data);
+        }
+    }, [data]);
+
+    const addPlant = (plant: Plant) => {
+        setPlants([...plants, plant]);
+    };
+
+    const removePlant = (id: string) => {
+        const plantsToKeep = plants.filter((item) => item.id !== id);
+
+        setPlants(plantsToKeep);
+    };
+
+    const updatePlant = (plant: Plant) => {
+        const i = plants.findIndex((item) => item.id === plant.id);
+
+        const plantsCopy = [...plants];
+        plantsCopy[i] = plant;
+
+        setPlants(plantsCopy);
+    };
+
+    return { plants, addPlant, removePlant, updatePlant };
+};
