5e7b335266ce8e09050b84d863d548b6677289e0
laurpe <laura.perttila@edu.bc.fi>
2022-07-25 18:03:57 +0300

    useAPIData uses axios

--- src/hooks/useAPIData.tsx
+++ src/hooks/useAPIData.tsx
@@ -1,21 +1,18 @@
 import { useEffect, useState } from "react";
 
+import axios from "axios";
+
 export const useAPIData = <T,>(url: string) => {
     const [data, setData] = useState<T | null>(null);
 
     useEffect(() => {
         const baseUrl = process.env.REACT_APP_API_BASE_URL as string;
 
-        const getData = async <T,>(url: string): Promise<T> => {
-            const response = await fetch(`${baseUrl}/${url}`);
-            return response.json() as Promise<T>;
-        };
-
         const fetchData = async () => {
             try {
-                const result = await getData<T>(url);
+                const response = await axios.get<T>(`${baseUrl}/${url}`);
 
-                setData(result);
+                setData(response.data);
             } catch (error) {
                 throw new Error("Could not fetch data");
             }
