7c1af286034d6a7b82f1e7fddb5dd4824a10573c
laurpe <laura.perttila@edu.bc.fi>
2022-07-26 17:04:35 +0300

    logout icon button

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,10 +5,12 @@ import Column from "./style/Generics/Column";
 import Row from "./style/Generics/Row";
 import Title from "./style/Generics/Title";
 import IconButton from "./style/Generics/IconButton";
+
 import CloseIcon from "@mui/icons-material/Close";
-import Button from "./style/Generics/Button";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 import styled from "styled-components";
+
 import { useUser } from "../hooks/useUser";
 
 const StyledRow = styled(Row)`
@@ -25,9 +27,17 @@ const Profile = () => {
             <Column height="100%" padding="0 0 40px 0">
                 <Row justifyContent="space-between">
                     <Title>Profile</Title>
-                    <IconButton type="button" onClick={() => navigate("/main")}>
-                        <CloseIcon sx={{ fontSize: 26 }} />
-                    </IconButton>
+                    <Row>
+                        <IconButton type="button" onClick={() => logout()}>
+                            <LogoutIcon sx={{ fontSize: 26 }} />
+                        </IconButton>
+                        <IconButton
+                            type="button"
+                            onClick={() => navigate("/main")}
+                        >
+                            <CloseIcon sx={{ fontSize: 26 }} />
+                        </IconButton>
+                    </Row>
                 </Row>
                 <Column padding="0 0 40px 0">
                     <StyledRow justifyContent="space-between">
@@ -35,9 +45,6 @@ const Profile = () => {
                         <div>{user.email}</div>
                     </StyledRow>
                 </Column>
-                <Button type="button" width="100%" onClick={() => logout()}>
-                    Log out
-                </Button>
             </Column>
         </Popup>
     );
