f4c48f2e4e90e2ce30a99731efcc776348218ef7
Laura Perttilä <laura@perttila.me>
2022-08-08 13:12:35 +0300

    bottom padding for main removed from body into column

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,13 @@
 import Plants from "./Plants";
 import Header from "./Header";
+import Column from "./style/Generics/Column";
 
 const Main = () => {
     return (
-        <>
+        <Column padding="0 0 60px 0">
             <Header />
             <Plants />
-        </>
+        </Column>
     );
 };
 
