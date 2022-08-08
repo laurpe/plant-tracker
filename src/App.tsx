ac33519810cb4461d70cf690d0cff34b6a08ef13
Laura Perttilä <laura@perttila.me>
2022-08-08 13:42:16 +0300

    scroll to top when navigating to different page

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ import AddGrowingMedium from "./components/AddGrowingMedium";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LogIn />} />
-                <Route path="/main" element={<Main />} />
-                <Route path="/add" element={<AddPlant />} />
-                <Route
-                    path="/add-growing-medium"
-                    element={<AddGrowingMedium />}
-                />
-                <Route path="/plants/:id" element={<SinglePlant />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
+            <ScrollToTop>
+                <Routes>
+                    <Route path="/" element={<LogIn />} />
+                    <Route path="/main" element={<Main />} />
+                    <Route path="/add" element={<AddPlant />} />
+                    <Route
+                        path="/add-growing-medium"
+                        element={<AddGrowingMedium />}
+                    />
+                    <Route path="/plants/:id" element={<SinglePlant />} />
+                    <Route path="/login" element={<LogIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </ScrollToTop>
         </BrowserRouter>
     );
 };
