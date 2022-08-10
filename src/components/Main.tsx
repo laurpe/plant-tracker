4748fce5328c6fad0b6a291957d36fd52d942385
Laura Perttilä <laura@perttila.me>
2022-08-10 17:24:47 +0300

    min height for main view

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,12 +2,20 @@ import Plants from "./Plants";
 import Header from "./Header";
 import Column from "./style/Generics/Column";
 
+import styled from "styled-components";
+
+const StyledColumn = styled(Column)`
+    padding: 0 0 60px 0;
+    background-color: rgb(235, 243, 241);
+    min-height: 100vh;
+`
+
 const Main = () => {
     return (
-        <Column padding="0 0 60px 0" backgroundColor="rgb(235, 243, 241)">
+        <StyledColumn>
             <Header />
             <Plants />
-        </Column>
+        </StyledColumn>
     );
 };
 
