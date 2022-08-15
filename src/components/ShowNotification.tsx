ec05bbbb47025197df3dee0b760a2c799b9ba226
Laura Perttilä <laura@perttila.me>
2022-08-15 11:16:20 +0300

    animation to show notification

--- src/components/ShowNotification.tsx
+++ src/components/ShowNotification.tsx
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import { Notification } from "../types";
+
+const animation = keyframes`
+    0% {
+        height: 0;
+    }
+    20% {
+        height: 80px;
+    }
+    80% {
+        height: 80px;
+    }
+    100% {
+        height: 0;
+    }
+`;
 
 const NotificationDiv = styled.div`
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
-    padding: 2rem;
+    height: 80px;
     background-color: white;
     display: flex;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
     font-size: 1rem;
     z-index: 1000;
     box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 3px 0.5px;
+    animation-name: ${animation};
+    animation-duration: 5s;
 `;
 
 interface Props {
