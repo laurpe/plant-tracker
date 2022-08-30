23c9e1c1e5805d8e21012d8ac320bc84472d4287
laurpe <laura.perttila@edu.bc.fi>
2022-08-30 15:40:33 +0300

    Search plant (#19)
    * search bar
    
    * search functionality

--- src/components/Plants.tsx
+++ src/components/Plants.tsx
@@ -3,6 +3,9 @@ import dayjs from "dayjs";
 import { Plant } from "../types";
 import PlantCard from "./PlantCard";
 import { usePlants } from "../hooks/usePlants";
+import { useState } from "react";
+
+import styled from "styled-components";
 
 const calculateNextWatering = (plant: Plant): dayjs.Dayjs => {
     const nextWatering = dayjs(plant.lastWatered).add(
@@ -13,28 +16,78 @@ const calculateNextWatering = (plant: Plant): dayjs.Dayjs => {
     return nextWatering;
 };
 
+const StyledInput = styled.input`
+    border: none;
+    border-radius: 10px;
+    background-color: white;
+    min-height: 1.5rem;
+    padding: 1rem;
+    font-family: inherit;
+    font-size: 1.2rem;
+    color: #225c55;
+    width: 100%;
+    margin: 1rem 0 1rem 0;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 3px 0.5px;
+
+    &::placeholder {
+        color: #7b9c99;
+    }
+`;
+
 const Plants = () => {
     const { plants, updatePlant } = usePlants();
+    const [search, setSearch] = useState<string>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
+
     return (
         <>
-            {plants
-                .sort((a, b) => {
-                    const aNextWatering = calculateNextWatering(a);
-                    const bNextWatering = calculateNextWatering(b);
+            <StyledInput
+                type="text"
+                placeholder="search plant"
+                value={search}
+                onChange={handleChange}
+            />
+            {search &&
+                plants
+                    .filter((plant) =>
+                        plant.name
+                            .toLowerCase()
+                            .includes(search.toLowerCase().trim())
+                    )
+                    .map((plant) => {
+                        const nextWatering = calculateNextWatering(plant);
+                        return (
+                            <PlantCard
+                                plant={plant}
+                                key={plant.id}
+                                nextWatering={nextWatering}
+                                updatePlant={updatePlant}
+                            />
+                        );
+                    })}
 
-                    return aNextWatering.isBefore(bNextWatering) ? -1 : 1;
-                })
-                .map((plant) => {
-                    const nextWatering = calculateNextWatering(plant);
-                    return (
-                        <PlantCard
-                            plant={plant}
-                            key={plant.id}
-                            nextWatering={nextWatering}
-                            updatePlant={updatePlant}
-                        />
-                    );
-                })}
+            {!search &&
+                plants
+                    .sort((a, b) => {
+                        const aNextWatering = calculateNextWatering(a);
+                        const bNextWatering = calculateNextWatering(b);
+
+                        return aNextWatering.isBefore(bNextWatering) ? -1 : 1;
+                    })
+                    .map((plant) => {
+                        const nextWatering = calculateNextWatering(plant);
+                        return (
+                            <PlantCard
+                                plant={plant}
+                                key={plant.id}
+                                nextWatering={nextWatering}
+                                updatePlant={updatePlant}
+                            />
+                        );
+                    })}
         </>
     );
 };
